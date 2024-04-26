e1020f139b1be436331ae078eacb263589b8db1c
Danstan <danstanochieng@gmail.com>
2024-04-26 12:47:41 +0300

    Files Uploaded

---
+++ js/main.js
@@ -0,0 +1,197 @@
+(function ($) {
+    "use strict";
+
+    // BACK TO TOP BUTTON
+    $(window).on("scroll", function (e) {
+        if ($(this).scrollTop() > 0) {
+            $('#back-to-top').fadeIn('slow');
+        } else {
+            $('#back-to-top').fadeOut('slow');
+        }
+    });
+    $(document).on("click", "#back-to-top", function (e) {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 0);
+        return false;
+    });
+
+
+    // COVER IMAGE
+    $(".cover-image").each(function () {
+        var attr = $(this).attr('data-bs-image-src');
+        if (typeof attr !== typeof undefined && attr !== false) {
+            $(this).css('background', 'url(' + attr + ') center center');
+        }
+    });
+
+    // MODAL
+    // SHOWING MODAL WITH EFFECT
+    $('.modal-effect').on('click', function (e) {
+        e.preventDefault();
+        var effect = $(this).attr('data-bs-effect');
+        $('#modaldemo8').addClass(effect);
+    });
+
+    // HIDE MODAL WITH EFFECT
+    $('#modaldemo8').on('hidden.bs.modal', function (e) {
+        $(this).removeClass(function (index, className) {
+            return (className.match(/(^|\s)effect-\S+/g) || []).join(' ');
+        });
+    });
+
+    // BY DEFAULT, BOOTSTRAP DOESN'T AUTO CLOSE POPOVER AFTER APPEARING IN THE PAGE 
+    $(document).on('click', function (e) {
+        $('[data-toggle="popover"],[data-original-title]').each(function () {
+            //the 'is' for buttons that trigger popups
+            //the 'has' for icons within a button that triggers a popup
+            if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                (($(this).popover('hide').data('bs.popover') || {}).inState || {}).click = false // fix for BS 3.3.6
+            }
+
+        });
+    });
+
+    // INPUT FILE BROWSER
+    $(document).on('change', '.file-browserinput', function () {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+    }); // We can watch for our custom `fileselect` event like this
+
+    // FILE UPLOAD
+    $('.file-browserinput').on('fileselect', function (event, numFiles, label) {
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+        if (input.length) {
+            input.val(log);
+        } else {
+            if (log) alert(log);
+        }
+    });
+
+
+        /******* Navigation Style *******/
+
+        let bodyhorizontal = $('body').hasClass('horizontal');
+        if (bodyhorizontal) {
+            if( !document.querySelector('.login-img') ){
+                ActiveSubmenu();
+                checkHoriMenu();
+                responsive();
+            }
+            if(window.innerWidth>=992){
+                let li = document.querySelectorAll('.side-menu li')
+                li.forEach((e, i) => {
+                    e.classList.remove('is-expanded')
+                })
+                var animationSpeed = 300;
+                // first level
+                var parent = $("[data-bs-toggle='sub-slide']").parents('ul');
+                var ul = parent.find('ul:visible').slideUp(animationSpeed);
+                ul.removeClass('open');
+                var parent1 = $("[data-bs-toggle='sub-slide2']").parents('ul');
+                var ul1 = parent1.find('ul:visible').slideUp(animationSpeed);
+                ul1.removeClass('open');
+            }
+            $('body').addClass('horizontal');
+            $(".main-content").addClass("hor-content");
+            $(".main-content").removeClass("app-content");
+            $(".main-container").addClass("container");
+            $(".main-container").removeClass("container-fluid");
+            $(".app-header").addClass("hor-header");
+            $(".hor-header").removeClass("app-header");
+            $(".app-sidebar").addClass("horizontal-main")
+            $(".main-sidemenu").addClass("container")
+            $('body').removeClass('sidebar-mini');
+            $('body').removeClass('sidenav-toggled');
+            $('body').removeClass('horizontal-hover');
+            $('body').removeClass('default-menu');
+            $('body').removeClass('icontext-menu');
+            $('body').removeClass('icon-overlay');
+            $('body').removeClass('closed-leftmenu');
+            $('body').removeClass('hover-submenu');
+            $('body').removeClass('hover-submenu1');
+            // // To enable no-wrap horizontal style
+            $('#slide-left').removeClass('d-none');
+            $('#slide-right').removeClass('d-none');
+            document.querySelector('.horizontal .side-menu')?.classList.add('flex-nowrap')
+            // To enable wrap horizontal style
+            // $('#slide-left').addClass('d-none');
+            // $('#slide-right').addClass('d-none');
+            // document.querySelector('.horizontal .side-menu').style.flexWrap = 'wrap'
+            
+        } 
+
+        //Datatable
+        $('#example2').DataTable({
+        responsive: true,
+        language: {
+            searchPlaceholder: 'Search...',
+            sSearch: '',
+            lengthMenu: '_MENU_ Items',
+            }
+        });
+
+        //Tooltip
+        $(function () {
+          $('[data-toggle="tooltip"]').tooltip()
+        });
+
+
+})(jQuery);
+
+// REPLY
+function replay() {
+    "use strict";
+
+    let replayButtom = document.querySelectorAll('.reply a')
+    // Creating Div
+    let Div = document.createElement('div')
+    Div.setAttribute('class', "comment mt-5 d-grid")
+    // creating textarea
+    let textArea = document.createElement('textarea')
+    textArea.setAttribute('class', "form-control")
+    textArea.setAttribute('rows', "5")
+    textArea.innerText = "Your Comment";
+    // creating Cancel buttons
+    let cancelButton = document.createElement('button');
+    cancelButton.setAttribute('class', "btn btn-danger");
+    cancelButton.innerText = "Cancel";
+
+    let buttonDiv = document.createElement('div')
+    buttonDiv.setAttribute('class', "btn-list ms-auto mt-2")
+
+    // Creating submit button
+    let submitButton = document.createElement('button');
+    submitButton.setAttribute('class', "btn btn-success ms-3");
+    submitButton.innerText = "Submit";
+
+    // appending text are to div
+    Div.append(textArea)
+    Div.append(buttonDiv);
+    buttonDiv.append(cancelButton);
+    buttonDiv.append(submitButton);
+
+    replayButtom.forEach((element, index) => {
+
+        element.addEventListener('click', () => {
+            let replay = $(element).parent()
+            replay.append(Div)
+
+            cancelButton.addEventListener('click', () => {
+                Div.remove()
+            })
+        })
+    })
+
+
+}
+replay()
+
+// OFF-CANVAS STYLE
+// $('.off-canvas').on('click', function () {
+//     $('body').addClass('overflow-y-scroll');
+//     $('body').addClass('pe-0');
+// });
