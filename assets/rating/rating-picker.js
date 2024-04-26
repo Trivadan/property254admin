72748708f67c312a347e669c94f6999e8641b08b
Danstan <danstanochieng@gmail.com>
2024-04-26 12:52:01 +0300

    assests uploaded

---
+++ assets/rating/rating-picker.js
@@ -0,0 +1,31 @@
+
+    
+    $.ratePicker("#rating-1", {
+        rate : function (stars){
+            alert('Sample 3\'s Rate is ' + stars);
+        }
+    });
+
+    $.ratePicker("#rating-2", {
+        rate : function (stars){
+            alert('Sample 3\'s Rate is ' + stars);
+        }
+    });
+
+    $.ratePicker("#rating-3", {
+        rate : function (stars){
+            alert('Sample 3\'s Rate is ' + stars);
+        }
+    });
+
+    $.ratePicker("#rating-4", {
+        rate : function (stars){
+            alert('Sample 3\'s Rate is ' + stars);
+        }
+    });
+
+    $.ratePicker("#rating-5", {
+        rate : function (stars){
+            alert('Sample 3\'s Rate is ' + stars);
+        }
+    });
